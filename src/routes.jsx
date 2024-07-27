a8f871a39c6e03a67cf9374df5b23c0ade81473a
alex.laycy <alexlaycy@gmail.com>
2024-07-27 14:01:05 +0100

    Updated routes file to utilise react-router-dom v6 techniques

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,27 +1,28 @@
-// src/routes.js
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./components/pages/LandingPage";
-import SignUpPage from "./components/pages/SignUpPage";
-import LoginPage from "./components/pages/LoginPage";
-import KitCreationPage from "./components/pages/KitCreationPage";
-import KitsPage from "./components/pages/KitsPage";
-import ProfilePage from "./components/pages/ProfilePage";
-import NotFoundPage from "./components/pages/NotFoundPage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import LandingPage from "./pages/LandingPage";
+import HandoverKitsPage from "./pages/HandoverKitsPage";
+import CreateHandoverKitPage from "./pages/CreateHandoverKitPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-const AppRoutes = () => (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route
-        path="/kits/create"
-        element={<KitCreationPage />}
-      />
-      <Route path="/kits" element={<KitsPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-);
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/kits"
+          element={<ProtectedRoute element={HandoverKitsPage} />}
+        />
+        <Route
+          path="/kits/create"
+          element={<ProtectedRoute element={CreateHandoverKitPage} />}
+        />
+      </Routes>
+  );
+}
 
-export default AppRoutes;
+export default App;
