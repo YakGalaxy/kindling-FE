fe6ec13b4c1538ad4ef03482ecd2143a097cb08e
alex.laycy <alexlaycy@gmail.com>
2024-08-10 11:15:24 +0100

    Updated to landing page

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { Button, Container, Typography, Box } from "@mui/material";
+import { Button, Container, Typography, Box, IconButton } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/header";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
 
 const LandingPage = () => {
   const navigate = useNavigate();
 
   const handleSignUp = () => {
     navigate("/signup");
+  };
+
+  const handleLogin = () => {
+    navigate("/login");
   };
 
   return (
@@ -25,20 +30,54 @@ const LandingPage = () => {
           p: 3,
         }}
       >
-        <Typography variant="h2" component="h1" gutterBottom>
-          Welcome to the Handover Creation Tool
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            mb: 3, // Adjust margin bottom for spacing
+          }}
+        >
+          <Typography
+            variant="h2"
+            component="h1"
+            sx={{
+              color: "#fff", // Ensure text color is white for better contrast
+              mr: 1, // Margin right to create space between text and icon
+            }}
+          >
+            Kindling
+          </Typography>
+          <IconButton
+            sx={{
+              color: "primary.main",
+            }}
+          >
+            <WhatshotIcon fontSize="large" />
+          </IconButton>
+        </Box>
+        <Typography variant="h5" component="p" sx={{ mb: 4 }}>
+          Create and manage handover kits easily.
         </Typography>
-        <Typography variant="h5" component="p" gutterBottom>
-          Create and manage your handover kits easily.
-        </Typography>
-        <Box mt={4}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
           <Button
             variant="contained"
             color="primary"
             size="large"
             onClick={handleSignUp}
+             // Margin-right for spacing between buttons
           >
             Sign Up
+          </Button>
+          <Typography variant="body1" sx={{ mx: 2 }}>
+            or
+          </Typography>
+          <Button
+            variant="outlined"
+            color="primary"
+            size="large"
+            onClick={handleLogin}
+          >
+            Login
           </Button>
         </Box>
       </Container>
