6e280284e62fdb1e2843856d1cfce9fd1385a9cd
alex.laycy <alexlaycy@gmail.com>
2024-08-10 09:56:34 +0100

    Tweaks to kits page

--- src/components/pages/KitsPage.jsx
+++ src/components/pages/KitsPage.jsx
@@ -9,7 +9,6 @@ import {
   CardContent,
   CardMedia,
   Box,
-  Button,
 } from "@mui/material";
 import HandoverKitService from "../../services/handoverKitService";
 import { useNavigate } from "react-router-dom";
@@ -54,95 +53,115 @@ const KitsPage = () => {
   return (
     <>
       <Header />
-      <Container maxWidth="lg" sx={{ pt: 4}}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Handover Kits
-        </Typography>
-        {error && (
-          <Alert severity="error" sx={{ mb: 2 }}>
-            {error}
-          </Alert>
-        )}
-        {loading ? (
-          <CircularProgress />
-        ) : (
-          <Box>
-            <Grid container spacing={4}>
-              {displayedKits.length > 0 ? (
-                displayedKits.map((kit) => (
-                  <Grid item xs={12} sm={6} md={4} lg={3} key={kit._id}>
-                    <Card
-                      sx={{
-                        cursor: "pointer",
-                        transition: "transform 0.3s, box-shadow 0.3s", // Added transition for smooth effect
-                        "&:hover": {
-                          transform: "scale(1.05)", // Scale up slightly on hover
-                          boxShadow: 6, // Increase shadow on hover
-                        },
-                      }}
-                      onClick={() => handleKitClick(kit._id)}
-                    >
-                      <CardMedia
-                        component="img"
-                        height="140"
-                        image={kit.imageUrl || "https://picsum.photos/150.webp?grayscale"}
-                        alt={kit.title}
-                      />
-                      <CardContent>
-                        <Typography variant="h6" component="div">
-                          {kit.title}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          {kit.description}
-                        </Typography>
-                      </CardContent>
-                    </Card>
-                  </Grid>
-                ))
-              ) : (
-                <Typography variant="body1" sx={{ mt: 2 }}>
-                  No handover kits available.
-                </Typography>
-              )}
-              {/* Update "Create New Kit" card */}
-              <Grid item xs={12} sm={6} md={4} lg={3}>
-                <Card
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    cursor: "pointer",
-                    backgroundColor: "#6a1b9a",
-                    color: "white",
-                    transition:
-                      "background-color 0.3s, transform 0.3s, box-shadow 0.3s", // Added transition for smooth effect
-                    "&:hover": {
-                      backgroundColor: "#4a148c",
-                      transform: "scale(1.05)", // Scale up slightly on hover
-                      boxShadow: 6, // Increase shadow on hover
-                    },
-                  }}
-                  onClick={handleCreateNewKit}
-                >
-                  <Box
+      <Container maxWidth="lg" sx={{ pt: 4 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center", // Centers the content horizontally
+          }}
+        >
+          <Typography
+            variant="h4"
+            component="h1"
+            gutterBottom
+            sx={{
+              alignSelf: "flex-start",
+              mb: 4,
+            }}
+          >
+            Your Kits
+          </Typography>
+          {error && (
+            <Alert severity="error" sx={{ mb: 2, width: "100%" }}>
+              {error}
+            </Alert>
+          )}
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <Box sx={{ width: "100%" }}>
+              <Grid container spacing={4}>
+                {displayedKits.length > 0 ? (
+                  displayedKits.map((kit) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} key={kit._id}>
+                      <Card
+                        sx={{
+                          cursor: "pointer",
+                          transition: "transform 0.3s, box-shadow 0.3s",
+                          "&:hover": {
+                            transform: "scale(1.05)",
+                            boxShadow: 6,
+                          },
+                        }}
+                        onClick={() => handleKitClick(kit._id)}
+                      >
+                        <CardMedia
+                          component="img"
+                          height="140"
+                          image={
+                            kit.imageUrl ||
+                            "https://picsum.photos/150.webp?grayscale"
+                          }
+                          alt={kit.title}
+                        />
+                        <CardContent>
+                          <Typography variant="h6" component="div">
+                            {kit.title}
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            {kit.description}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  ))
+                ) : (
+                  <Typography variant="body1" sx={{ mt: 2 }}>
+                    No handover kits available.
+                  </Typography>
+                )}
+                {/* Update "Create New Kit" card */}
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                  <Card
                     sx={{
-                      p: 2,
                       display: "flex",
-                      justifyContent: "center",
+                      flexDirection: "column",
                       alignItems: "center",
+                      cursor: "pointer",
+                      backgroundColor: "#6a1b9a",
+                      color: "white",
+                      transition:
+                        "background-color 0.3s, transform 0.3s, box-shadow 0.3s",
                       height: "100%",
-                      textAlign: "center",
+                      "&:hover": {
+                        backgroundColor: "#4a148c",
+                        transform: "scale(1.05)",
+                        boxShadow: 6,
+                      },
                     }}
+                    onClick={handleCreateNewKit}
                   >
-                    <Typography variant="h6" component="div">
-                      Create a new Kit
-                    </Typography>
-                  </Box>
-                </Card>
+                    <Box
+                      sx={{
+                        p: 2,
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "100%",
+                        textAlign: "center",
+                      }}
+                    >
+                      <Typography variant="h6" component="div">
+                        Create a new Kit
+                      </Typography>
+                    </Box>
+                  </Card>
+                </Grid>
               </Grid>
-            </Grid>
-          </Box>
-        )}
+            </Box>
+          )}
+        </Box>
       </Container>
     </>
   );
